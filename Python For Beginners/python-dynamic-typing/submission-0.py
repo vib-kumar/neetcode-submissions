c9c344933a88307d92e761b8ecc2d940381cd9ef
Vibhav Kumar <66163582+vib-kumar@users.noreply.github.com>
2026-05-07 21:48:09 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1987
+print(type(variable))
+variable = 2.17
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Birthday"
+print(type(variable))
+variable = ["I", "am", "happy"]
+print(type(variable))
