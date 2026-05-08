d5f46e496b3d8f778271792ef8c0965d4b79718e
Vibhav Kumar <66163582+vib-kumar@users.noreply.github.com>
2026-05-07 21:45:53 -0500

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 17
+float_type = 3.14
+bool_type = True
+string_type = "pie"
+list_type = [1, 2, "abc"]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
